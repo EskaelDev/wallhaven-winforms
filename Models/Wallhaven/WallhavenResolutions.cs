5d09e4d8ee18ee4e112056203761cb65ee702f6d
tomek <rainbowvme@gmail.com>
2020-08-17 00:36:10 +0200

    Moved wallhaven models to separate directory.

---
+++ Models/Wallhaven/WallhavenResolutions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wallpaper_forms.Models
+{
+    public static class WallhavenResolutions
+    {
+        public static string _2560x1080 => "2560x1080";
+        public static string _3440x1440 => "3440x1440 ";
+        public static string _3840x1600 => "3840x1600 ";
+        public static string _1280x720 => "1280x720";
+        public static string _1600x900 => "1600x900";
+        public static string _1920x1080 => "1920x1080 ";
+        public static string _2560x1440 => "2560x1440 ";
+        public static string _3840x2160 => "3840x2160 ";
+        public static string _1280x800 => "1280x800";
+        public static string _1600x1000 => "1600x1000 ";
+        public static string _1920x1200 => "1920x1200 ";
+        public static string _2560x1600 => "2560x1600 ";
+        public static string _3840x2400 => "3840x2400 ";
+        public static string _1280x960 => "1280x960";
+        public static string _1600x1200 => "1600x1200 ";
+        public static string _1920x1400 => "1920x1400 ";
+        public static string _2560x1920 => "2560x1920 ";
+        public static string _3840x2880 => "3840x2880 ";
+        public static string _1280x1024 => "1280x1024 ";
+        public static string _1600x1280 => "1600x1280 ";
+        public static string _1920x1536 => "1920x1536 ";
+        public static string _2560x2048 => "2560x2048 ";
+        public static string _3840x3072 => "3840x3072";
+    }
+}
